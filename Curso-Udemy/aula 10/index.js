2b8219ae47ed8c8d2b48b350ae0fc2d471baa4d3
Lucas-Felix <lucas.felix@bandtec.com.br>
2021-06-13 16:52:55 -0300

    aula 10 ao 12

--- Curso-Udemy/aula 10/index.js
+++ Curso-Udemy/aula 10/index.js
@@ -4,17 +4,28 @@
 % = Resto da divisão
 
 */
-const num1 = 5;
-const num2 = 2;
-const num3 = 10;
-console.log((num1 + num2) * num3);
+// const num1 = 5;
+// const num2 = 2;
+// const num3 = 10;
+// console.log((num1 + num2) * num3);
 
-let contador = 1;
-contador++; //2
-contador++; // 3
-contador++; // 4
-contador++; // 5
-console.log(contador);
+// let contador = 1;
+// contador++; //2
+// contador++; // 3
+// contador++; // 4
+// contador++; // 5
+// console.log(contador);
 
+// const passo = 50;
+// let contador = 0;
+// contador += 2; // contador = contador + 50
+// contador += 2;
+// contador += 2;
 
+// console.log(contador)
 
+const num1= 10;
+const num2= Number('5.2');
+console.log(num1 + num2);
+console.log(typeof(num1 * num2));
+console.log(typeof(num2));
---
+++ Curso-Udemy/aula 12/index.js
@@ -0,0 +1,14 @@
+// Exercício aula 12
+
+let varA = 'A'; // B
+let varB = 'B'; // C
+let varC = 'C'; // A
+
+const varATemp = varA;
+
+varA = varB;
+varB = varC;
+varC = varATemp;
+
+
+console.log(varA, varB, varC)
