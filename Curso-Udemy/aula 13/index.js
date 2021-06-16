23e10a4482f18b09e7a244d63a834a20a30ee38f
Lucas-Felix <lucas.felix@bandtec.com.br>
2021-06-16 18:41:50 -0300

    aula 13

---
+++ Curso-Udemy/aula 13/index.js
@@ -0,0 +1,10 @@
+//               01234567
+let umaString = "Um texto";
+console.log(umaString.charAt(3));
+console.log(umaString.concat(" em um lindo dia."));
+console.log(umaString + 'em um lindo dia.');
+console.log(`${umaString} em um lindo dia. `);
+console.log(umaString.indexOf('o', 3));
+console.log(umaString.lastIndexOf('m', 3));
+console.log(umaString.length);
+console.log(umaString.slice(0, 4));
