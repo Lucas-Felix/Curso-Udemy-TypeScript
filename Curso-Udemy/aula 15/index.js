841c09f0b6c95878b24f59ff49157b969dbe9a64
lucasgfelix <lucas.felix@bandtec.com.br>
2021-10-25 11:48:54 -0300

    Aula 15. Algumas coisas sobre Math

---
+++ Curso-Udemy/aula 15/index.js
@@ -0,0 +1,11 @@
+let num1 = 9;
+console.log(num1 ** 0.5); // Equação para pegar raiz quadrada de algum numero.
+
+// console.log(100 / 0) = Infinity = valor true
+// let num2 = Math.floor(num1);  Arredondando para baixo num1
+// let num2 = Math.ceil(num1);  Arredondando para cima num1
+// let num2 = Math.round(num1); Arredonda para cima com os números a cima da metade;
+
+// console.log(Math.max(1, 2, 3, 4, 5, -10, -50, 1500, 9, 7));  Math.max = Maior numero da sequencia
+// console.log(Math.min(1, 2, 3, 4, 5, -10, -50, 1500, 9, 7));  Math.min = Menor numero da sequencia
+// const aleatório = Math.R(Math.random() * (10 - 5) + 5);  Numero aleatório entre 10 e 5 arrendados
