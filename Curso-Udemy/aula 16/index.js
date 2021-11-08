e3fe4aeaebfdd63d346bdb5375ce65533f1df72a
lucasgfelix <lucas.felix@bandtec.com.br>
2021-11-08 09:59:48 -0300

    exercicio aula 16

--- Curso-Udemy/aula 16/index.js
+++ Curso-Udemy/aula 16/index.js
@@ -4,5 +4,11 @@ const texto = document.getElementById('texto');
 
 
 numeroTitulo.innerHTML = numero;
-texto.innerHTML = `<p>Raiz quadrada ${numero ** 5}</p>`;
-texto.innerHTML = `<p>${numero} é inteiro ${Number.isInteger(numero)}p>`;
+
+texto.innerHTML = '';
+texto.innerHTML += `<p>A Raiz quadrada: ${numero ** 5}.</p>`;
+texto.innerHTML += `<p>O ${numero} é inteiro?  ${Number.isInteger(numero)}.</p>`;
+texto.innerHTML += `<p>O ${numero} é NaN? ${Number.isNaN(numero)}.</p>`;
+texto.innerHTML += `<p>Arredondado para baixo: ${Math.floor(numero)}.</p>`;
+texto.innerHTML += `<p>Arredondado para cima: ${Math.ceil(numero)}.</p>`;
+texto.innerHTML += `<p>Com duas casas decimais: ${numero.toFixed(2)}.</p>`;
